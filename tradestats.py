b6485980eb43fa8963c2df4ac207c70414c8863d
Josh Watley <watleyjosh@gmail.com>
2023-06-09 22:23:10 +0100

    Average trade time by outcome

--- tradestats.py
+++ tradestats.py
@@ -9,22 +9,53 @@ def main():
     # instead of reading from file every time
     trades = performance.tradeJSON
     # print(trades)
-    avgtradetime = getavgtradetime(trades)
+
+    trades = determinoutcome(trades)
+    avgtradetime = getavgtradetime(trades, True)
+    print(avgtradetime)
+    avgtradetime = getavgtradetime(trades, False)
     print(avgtradetime)
 
-def getavgtradetime(trades):
+
+def determinoutcome(trades):
+    for trade in trades:
+        if (trade.get('swap') + trade.get('profit') > 100):
+            trade.update({'win':'True'})
+        elif (trade.get('swap') + trade.get('profit') < -25):
+            trade.update({'win':'False'})
+        else:
+            trade.update({'win':'NA'})
+            
+    return trades
+
+
+def getavgtradetime(trades, winning_trade):
     time_format = "%Y.%m.%d %H:%M:%S"
     diff=0
-    ntrades = len(trades)
+    ntrades=0
+    if winning_trade: 
+        print("Averaging winning trades")
+    else:
+        print("Averaging losing trades")
+
     for trade in trades:
         t1 = dt.strptime(trade.get('created'), time_format)
         t2 = dt.strptime(trade.get('closed'), time_format)
         delta = t2-t1
-        print(f"Created: {trade.get('created')} Closed: {trade.get('closed')} - Diff: {delta.total_seconds()} - Profit: {trade.get('profit')}")
-        diff = diff + delta.total_seconds()
-        
-    return diff / ntrades
 
+        if winning_trade:
+            if trade.get('win') == "True": # Average winning trades
+                ntrades+=1
+                print(f"Created: {trade.get('created')} Closed: {trade.get('closed')} - Diff: {delta.total_seconds()} - Profit: {trade.get('profit')}")
+                diff = diff + delta.total_seconds()
+        else:
+            if trade.get('win') == "False": # Average losing trades
+                ntrades+=1
+                print(f"Created: {trade.get('created')} Closed: {trade.get('closed')} - Diff: {delta.total_seconds()} - Profit: {trade.get('profit')}")
+                diff = diff + delta.total_seconds()
+
+    print(f"Number of Trades: {ntrades:}") 
+    return diff / ntrades
 
 
 def readTradeCSVFile(inputfile):
@@ -55,6 +86,7 @@ def readTradeCSVFile(inputfile):
             print(f'Error: {e}')
     
     return trades
+
 
 if __name__ == "__main__":
     main()
