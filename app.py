89bc846e308104ef463693d8f04ded31fdb2c7d3
Josh Watley <watleyjosh@gmail.com>
2023-06-10 17:17:05 +0100

    Heroku fix

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, jsonify, request
 import mysql.connector
-import server_settings_dev as dev
+# import server_settings_dev as dev
 import traceback
 
-conn = mysql.connector.connect(user=dev.username, password=dev.password, host=dev.endpoint, database=dev.database)
+# conn = mysql.connector.connect(user=dev.username, password=dev.password, host=dev.endpoint, database=dev.database)
 
 app = Flask(__name__)
 
@@ -11,87 +11,87 @@ app = Flask(__name__)
 def home():
     return "Trade Tools Running."
 
-@app.route("/")
-def get_trade():
-    accountID = request.args.get('accountID')
+# @app.route("/")
+# def get_trade():
+#     accountID = request.args.get('accountID')
 
-    return jsonify(get_trades(accountID)), 200
-
-
-@app.route('/newtrade', methods=['POST'])
-def add_new_trade():
-    trade = request.get_json()
-    trade_save(trade)
-
-    return 'New Trade Saved', 200
+#     return jsonify(get_trades(accountID)), 200
 
 
-@app.route('/bulktrades', methods=['POST'])
-def bulk_upload_trades():
-    trades = request.get_json()
-    bulk_save_trades(trades)
+# @app.route('/newtrade', methods=['POST'])
+# def add_new_trade():
+#     trade = request.get_json()
+#     trade_save(trade)
 
-    return 'Bulk Trades Upload', 200
-
-
-def get_trades(accountID):
-    cursor = conn.cursor()
-    sql = """
-    SELECT ticketID, accountID, tradeType, symbol, price, sl, tp, swap, profit, closed, opened, outcome
-    FROM trades_tb
-    WHERE accountID=%s
-    """
-    val = accountID
-
-    cursor.execute(sql, (val,))
-    res = cursor.fetchall()
-    cursor.close()
-    return res
+#     return 'New Trade Saved', 200
 
 
-def trade_save(trade):
-    cursor = conn.cursor()
+# @app.route('/bulktrades', methods=['POST'])
+# def bulk_upload_trades():
+#     trades = request.get_json()
+#     bulk_save_trades(trades)
 
-    sql = """
-    INSERT INTO trades_tb 
-    (ticketID, accountID, tradeType, symbol, price, sl, tp, swap, profit, closed, opened, outcome)
-    VALUES
-    (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-    """
+#     return 'Bulk Trades Upload', 200
 
-    val = trade.get("ticketID"),trade.get("accountID"),trade.get("type"),trade.get("symbol"),trade.get("price"),trade.get("sl"),trade.get("tp"),trade.get("swap"),trade.get("profit"),trade.get("closed"),trade.get("created"), trade.get("outcome")
 
-    try:
-        cursor.execute(sql, val)
-        conn.commit()
-        cursor.close()
-    except Exception:
-        print(traceback.format_exc())
-        conn.rollback()
-        cursor.close()
+# def get_trades(accountID):
+#     cursor = conn.cursor()
+#     sql = """
+#     SELECT ticketID, accountID, tradeType, symbol, price, sl, tp, swap, profit, closed, opened, outcome
+#     FROM trades_tb
+#     WHERE accountID=%s
+#     """
+#     val = accountID
 
-def bulk_save_trades(trades):
-    cursor = conn.cursor()
+#     cursor.execute(sql, (val,))
+#     res = cursor.fetchall()
+#     cursor.close()
+#     return res
 
-    val_trades = []
-    for trade in trades:
-         val_trades.append((trade.get("ticketID"),trade.get("accountID"),trade.get("type"),trade.get("symbol"),trade.get("price"),trade.get("sl"),trade.get("tp"),trade.get("swap"),trade.get("profit"),trade.get("closed"),trade.get("created"), trade.get("outcome")))
 
-    sql = """
-    INSERT INTO trades_tb 
-    (ticketID, accountID, tradeType, symbol, price, sl, tp, swap, profit, closed, opened, outcome)
-    VALUES
-    (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-    """
+# def trade_save(trade):
+#     cursor = conn.cursor()
 
-    try: 
-        cursor.executemany(sql, val_trades)
-        conn.commit()
-        cursor.close()
-    except Exception:
-        print(traceback.format_exc())
-        conn.rollback()
-        cursor.close()
+#     sql = """
+#     INSERT INTO trades_tb 
+#     (ticketID, accountID, tradeType, symbol, price, sl, tp, swap, profit, closed, opened, outcome)
+#     VALUES
+#     (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+#     """
+
+#     val = trade.get("ticketID"),trade.get("accountID"),trade.get("type"),trade.get("symbol"),trade.get("price"),trade.get("sl"),trade.get("tp"),trade.get("swap"),trade.get("profit"),trade.get("closed"),trade.get("created"), trade.get("outcome")
+
+#     try:
+#         cursor.execute(sql, val)
+#         conn.commit()
+#         cursor.close()
+#     except Exception:
+#         print(traceback.format_exc())
+#         conn.rollback()
+#         cursor.close()
+
+# def bulk_save_trades(trades):
+#     cursor = conn.cursor()
+
+#     val_trades = []
+#     for trade in trades:
+#          val_trades.append((trade.get("ticketID"),trade.get("accountID"),trade.get("type"),trade.get("symbol"),trade.get("price"),trade.get("sl"),trade.get("tp"),trade.get("swap"),trade.get("profit"),trade.get("closed"),trade.get("created"), trade.get("outcome")))
+
+#     sql = """
+#     INSERT INTO trades_tb 
+#     (ticketID, accountID, tradeType, symbol, price, sl, tp, swap, profit, closed, opened, outcome)
+#     VALUES
+#     (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+#     """
+
+#     try: 
+#         cursor.executemany(sql, val_trades)
+#         conn.commit()
+#         cursor.close()
+#     except Exception:
+#         print(traceback.format_exc())
+#         conn.rollback()
+#         cursor.close()
 
 
 if __name__ == '__main__':
